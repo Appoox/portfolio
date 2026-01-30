0bb5c1b86e6941a7ebc465678419a270c6e9948f
Appoox <gadha1998@gmail.com>
2026-01-30 12:29:28 +0530

    Used Claude to make a front-end

---
+++ base/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.ProfileDetailView.as_view(), kwargs={'pk':1}, name = 'profile')
+]
--- base/views.py
+++ base/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views.generic import DetailView
+from .models import *
 
 # Create your views here.
+
+class ProfileDetailView(DetailView):
+    model = Profile
+    template_name = "home.html"
+    context_object_name = "profile"
+    
